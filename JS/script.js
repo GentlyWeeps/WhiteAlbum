e0c4c10fbf3ea8ce07d49bac0394a15de09f08ba
GentlyWeeps <47528799+GentlyWeeps@users.noreply.github.com>
2019-03-21 09:44:11 -0400

    js2

--- JS/script.js
+++ JS/script.js
@@ -15,9 +15,9 @@ function largerNumber(){
 	}
 }
 function productNegative() {
-	var num1=prompt("What is the first number:");
-	var num2=prompt("What is the second number:");
-	var num3=prompt("What is the third number:");
+	var num1=parseInt(prompt("Enter the first number:"));
+	var num2=parseInt(prompt("Enter the second number:"));
+	var num3=parseInt(prompt("Enter the third number:"));
 	var product=num1+num2+num3;
 	if (product > 0) {
 		alert("The product is positive");
@@ -26,11 +26,12 @@ function productNegative() {
 		alert("The product is negative");
 	}
 }
-//		function order() {
-//	var num1,num2,num3,num4
-//	for(var i = 1; i <= 4; i++) {
-//		var = num+i
-//		prompt("What is number " + i);
-//	}
-	
-//}
+function order() {
+	var num1=parseInt(prompt("Enter the first number:"));
+	var num2=parseInt(prompt("Enter the second number:"));
+	var num3=parseInt(prompt("Enter the third number:"));
+	if(num1>num2 && num1>num3) {
+		alert(num1 + "," + num2 + "," + num3);
+	}
+	if()
+}
